6968ed808c9e230ddfdce8685fa2a2ce5d6ee343
Nikunj Dixit <nikunjdixit06@gmail.com>
2023-07-06 15:20:42 -0400

    Initial Commit

---
+++ Landmarks/LandmarksApp.swift
@@ -0,0 +1,17 @@
+//
+//  LandmarksApp.swift
+//  Landmarks
+//
+//  Created by nikunj dixit on 7/6/23.
+//
+
+import SwiftUI
+
+@main
+struct LandmarksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
